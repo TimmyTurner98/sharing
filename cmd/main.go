5f6d82addef05ace6329026de4a76e333e609df7
TimmyTurner <suleimenovt.98@gmail.com>
2025-06-02 17:14:59 +0500

    структура

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
+)
 
+func main() {
+	logrus.SetFormatter(new(logrus.JSONFormatter))
+
+	if err := godotenv.Load(); err != nil {
+		logrus.Fatalf("error loading env variables: %s", err.Eror())
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/TimmyTurner98/sharing
 
 go 1.21.4
+
+require (
+	github.com/jmoiron/sqlx v1.4.0
+	github.com/joho/godotenv v1.5.1
+	github.com/sirupsen/logrus v1.9.3
+)
+
+require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
