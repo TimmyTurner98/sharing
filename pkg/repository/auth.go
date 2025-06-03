349e316d9c3c290950a7272e782c99d5b727843e
TimmyTurner <suleimenovt.98@gmail.com>
2025-06-03 15:23:10 +0500

    test

--- pkg/repository/auth.go
+++ pkg/repository/auth.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"github.com/TimmyTurner98/sharing/models"
 	"github.com/jmoiron/sqlx"
 )
 
@@ -10,4 +11,13 @@ type AuthPostgres struct {
 
 func NewAuthPostgres(db *sqlx.DB) *AuthPostgres {
 	return &AuthPostgres{db: db}
+}
+
+func (r *AuthPostgres) CreateUser(user models.User) (int, error) {
+	var id int
+	err := r.db.QueryRow("INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id", user.Username, user.Password).Scan(&id)
+	if err != nil {
+		return 0, err
+	}
+	return id, nil
 }
