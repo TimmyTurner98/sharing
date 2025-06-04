fcd2555a3b2bc3c9e48e00dacaf672f910ed1746
TimmyTurner <suleimenovt.98@gmail.com>
2025-06-04 09:48:41 +0500

    fix push

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"os"
+
+	"github.com/TimmyTurner98/sharing/pkg/repository"
 	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
 )
@@ -9,6 +12,22 @@ func main() {
 	logrus.SetFormatter(new(logrus.JSONFormatter))
 
 	if err := godotenv.Load(); err != nil {
-		logrus.Fatalf("error loading env variables: %s", err.Eror())
+		logrus.Fatalf("error loading env variables: %s", err.Error())
 	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		Username: os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   os.Getenv("DB_NAME"),
+		SSLMode:  os.Getenv("DB_SSLMODE"),
+	})
+
+	if err != nil {
+		logrus.Fatalf("failed to initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
+
 }
