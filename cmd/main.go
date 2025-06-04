9e324acf1c7688989c122ce763355e659378f417
TimmyTurner <suleimenovt.98@gmail.com>
2025-06-04 18:09:28 +0500

    update

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"os"
 
+	"github.com/TimmyTurner98/sharing"
+	"github.com/TimmyTurner98/sharing/pkg/handler"
 	"github.com/TimmyTurner98/sharing/pkg/repository"
+	"github.com/TimmyTurner98/sharing/pkg/service"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
 )
@@ -29,5 +33,15 @@ func main() {
 	}
 
 	repos := repository.NewRepository(db)
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+	router := gin.Default()
+
+	s := &sharing.Server{}
+
+	if err := s.Run("8080", router); err != nil {
+		logrus.Fatalf("Ошибка запуска сервера: %s", err.Error())
+	}
 
 }
---
+++ pkg/handler/handler.go
@@ -0,0 +1,19 @@
+package handler
+
+import (
+	"github.com/TimmyTurner98/sharing/pkg/service"
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	services *service.Service
+}
+
+func NewHandler(services *service.Service) *Handler {
+	return &Handler{services: services}
+}
+
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.New()
+	router.Use(gin.Recovery(), gin.Logger())
+}
